416ae164453ebba790aa10b5a066cb062a252ea6
fahu <fahu@outlook.com>
2019-04-13 20:09:14 +0200

    Implement basic version of top navbar.
     - Add a placeholder icon as logo (it's a stock image)
     - Show name in navbar.
     - Show items defined defined in wordpress primary menu.

--- web/app/themes/coffee-cup/resources/views/partials/header.blade.php
+++ web/app/themes/coffee-cup/resources/views/partials/header.blade.php
@@ -1,10 +1,38 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+  <nav class="navbar navbar-expand-sm navbar-light">
+    <div class="container">
+      <a class="navbar-brand align-self-center" href="{{ home_url('/') }}">
+        <img src="@asset('images/logo/coffee-cat-stock-icon-navbar.png')" height="50px" alt="The logo of coffee cup" />
+        <span>{{ get_bloginfo("name", "display") }}</span>
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        @if (has_nav_menu('primary_navigation'))
+        {!!
+          wp_nav_menu( array(
+            'theme_location'  => 'primary_navigation',
+            'depth'	          => 2,
+            'container'       => 'div',
+            'container_class' => 'collapse navbar-collapse',
+            'container_id'    => 'bs-example-navbar-collapse-1',
+            'menu_class'      => 'navbar-nav ml-auto',
+            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'          => new WP_Bootstrap_Navwalker(),
+          ) );
+        !!}
+        @endif
+      </div>
+    </div>
+  </nav>
+  <nav class="nav-primary"></nav>
 </header>
