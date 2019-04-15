63e32f734cd3f674f3447e502bb3844500414329
Marlene <marlene.grabmair@gmail.com>
2019-04-15 21:04:03 +0200

    Implement basic card style.
    - Show Date, Title, Excerpt and Location.
    - Tags are used as Location.

--- web/app/themes/coffee-cup/resources/views/index.blade.php
+++ web/app/themes/coffee-cup/resources/views/index.blade.php
@@ -13,7 +13,7 @@
 <div class="container">
   <div class="row posts-grid">
     @while (have_posts()) @php the_post() @endphp
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 post-card-wrapper">
+      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 post-card-wrapper">
         @include('partials.content-'.get_post_type())
       </div>
    
--- web/app/themes/coffee-cup/resources/views/partials/content.blade.php
+++ web/app/themes/coffee-cup/resources/views/partials/content.blade.php
@@ -1,11 +1,17 @@
-<article @php post_class() @endphp>
+<article @php post_class() 
+@endphp>
   <div class="card mb-4">
-    <img src="..." class="card-img-top" alt="...">
+    @if (get_the_tags() > 0)
+    <div class="badge badge-primary">{{get_the_tags()[0]->name}}</div>
+
+    @endif {{the_post_thumbnail('post-thumbnail', array( 'class' => 'card-img-top' ))}}
     <div class="card-body">
       <h2 class="card-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-  @include('partials/entry-meta')
-      <p class="card-text"> @php the_excerpt() @endphp</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <div class="card-text"> @php the_excerpt() 
+@endphp</div>
+    </div>
+    <div class="card-footer text-muted">
+      <small class="text-muted">{{get_the_date( 'F, Y' )}}</small>
     </div>
   </div>
 </article>
