d00fe71ed4a5a0b36c70cb10441fd4dd0bf3e6f0
fahu <fahu@outlook.com>
2019-05-12 19:25:47 +0200

    Change posts grid to 2 columns side-by-side on md.

--- web/app/themes/coffee-cup/resources/views/index.blade.php
+++ web/app/themes/coffee-cup/resources/views/index.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 
 @section('content')
   @include('partials.page-header')
-  
+
   @if (!have_posts())
     <div class="alert alert-warning">
       {{ __('Sorry, no results were found.', 'sage') }}
@@ -13,10 +13,10 @@
 <div class="container">
   <div class="row posts-grid">
     @while (have_posts()) @php the_post() @endphp
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 post-card-wrapper">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 post-card-wrapper">
         @include('partials.content-'.get_post_type())
       </div>
-   
+
     @endwhile
   </div>
 </div>
