8ede3510f680bc58fd32312d7a9b1599c278e972
fahu <fahu@outlook.com>
2019-05-12 19:28:44 +0200

    Link to post from post card images.

--- web/app/themes/coffee-cup/resources/views/partials/content.blade.php
+++ web/app/themes/coffee-cup/resources/views/partials/content.blade.php
@@ -1,13 +1,15 @@
-<article @php post_class() 
+<article @php post_class()
 @endphp>
   <div class="card mb-4">
     @if (get_the_tags() > 0)
     <div class="badge badge-primary">{{get_the_tags()[0]->name}}</div>
-
-    @endif {{the_post_thumbnail('post-thumbnail', array( 'class' => 'card-img-top' ))}}
+    @endif
+    <a href="{{ get_permalink() }}">
+      {{the_post_thumbnail('post-thumbnail', array( 'class' => 'card-img-top' ))}}
+    </a>
     <div class="card-body">
       <h2 class="card-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-      <div class="card-text"> @php the_excerpt() 
+      <div class="card-text"> @php the_excerpt()
 @endphp</div>
     </div>
     <div class="card-footer text-muted">
