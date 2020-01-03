e8af425c464f8b3bc1c702c284302e7f9b547d8a
Fabian Hurnaus <fahu@outlook.com>
2020-01-04 00:21:59 +0100

    Add bootstrap.js.

--- web/app/themes/coffee-cup/app/setup.php
+++ web/app/themes/coffee-cup/app/setup.php
@@ -12,6 +12,7 @@ use Roots\Sage\Template\BladeProvider;
  */
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
+    wp_enqueue_script('sage/bootstrap.js', asset_path('../node_modules/bootstrap/dist/js/bootstrap.js'), ['jquery'], null, true);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
